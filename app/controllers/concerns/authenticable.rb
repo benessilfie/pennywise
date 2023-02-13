61f283c03b1fc7f676dfc46a7f30e5768bc1c887
benessilfie <bquansah007@gmail.com>
2023-02-13 22:05:01 +0000

    Cleaned up authenticable module to reflect  routing

--- app/controllers/concerns/authenticable.rb
+++ app/controllers/concerns/authenticable.rb
@@ -6,17 +6,6 @@ module Authenticable
     @current_user ||= User.find(decoded[:user_id])
   end
 
-  def check_user
-    return if @user&.id == current_user&.id
-
-    render json: { errors: ['You are not authorized to perform this action'] },
-           status: :unauthorized
-  end
-
-  def set_user
-    @user = User.find(params[:id])
-  end
-
   def destroy_user_session
     @current_user = nil
   end
