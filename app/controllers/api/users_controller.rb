2ef80b4e6d461b019c674d1ca4d420e161d784af
benessilfie <bquansah007@gmail.com>
2023-02-13 02:30:40 +0000

    Implemented authorization for user update action

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,11 +1,13 @@
 class Api::UsersController < ApiController
+  before_action :set_user, only: %i[show update]
+  before_action :check_user, only: %i[update]
+
   def index
-    @users = User.all.order(:id)
+    @users = User.all.order(id: :asc)
     render json: @users, status: :ok
   end
 
   def show
-    @user = User.find(params[:id])
     render json: @user, status: :ok
   end
 
@@ -13,15 +15,32 @@ class Api::UsersController < ApiController
     @user = User.create(user_params)
 
     if @user.valid?
-      render json: { message: 'User created successfully' }, status: :created
+      render json: {
+        message: 'User created successfully',
+        data: @user
+      }, status: :ok
     else
       render json: { errors: @user.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if @user.update(user_params)
+      render json: {
+        message: 'User updated successfully',
+        data: @user.as_json(only: %i[first_name last_name email dob address updated_at])
+      }, status: :ok
+    else
+      render json: {
+        message: 'User update failed',
+        errors: @user.errors.full_messages
+      }, status: :unprocessable_entity
     end
   end
 
   private
 
   def user_params
-    params.permit(:first_name, :last_name, :email, :password, :dob, :address).compact
+    params.permit(:id, :first_name, :last_name, :email, :password, :dob, :address).compact
   end
 end
