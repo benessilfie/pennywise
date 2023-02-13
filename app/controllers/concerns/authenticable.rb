7d39ff9430cec2bcbdd5f107ac2b2b5a8943ac0a
benessilfie <bquansah007@gmail.com>
2023-02-13 02:28:01 +0000

    Added method to authenticable module to handle decoding token to set current user

--- app/controllers/concerns/authenticable.rb
+++ app/controllers/concerns/authenticable.rb
@@ -2,10 +2,7 @@ module Authenticable
   def current_user
     return @current_user if @current_user
 
-    token = request.headers['Authorization'] & gsub('Bearer ', '')
-    return render json: { errors: ['Invalid token'] }, status: :unauthorized if token.blank?
-
-    decoded = JsonWebToken.decode(token)
+    decoded = decode_token
     @current_user ||= User.find(decoded[:user_id])
   end
 
@@ -22,5 +19,17 @@ module Authenticable
 
   def destroy_user_session
     @current_user = nil
+  end
+
+  private
+
+  def decode_token
+    token = request.headers['Authorization']&.split&.last
+    decoded = JsonWebToken.decode(token)
+
+    raise JWT::DecodeError if decoded.nil?
+    raise JWT::ExpiredSignature if Time.now.to_i >= decoded[:exp]
+
+    decoded
   end
 end
