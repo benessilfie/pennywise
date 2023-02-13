b80f03ac56e560db3bbc7e53b7d1ff35cb815af4
benessilfie <bquansah007@gmail.com>
2023-02-13 02:23:22 +0000

    Added more error cases to handle invalid or missing tokens

--- lib/error_handler.rb
+++ lib/error_handler.rb
@@ -3,21 +3,32 @@ module ErrorHandler
     case error
     when ActionController::ParameterMissing
       render json: { error: respond_with(error.message) }, status: :bad_request
+    when ActiveRecord::RecordNotFound
+      render json: { errors: ["User Record Not Found: No user found with ID: #{params[:id]}"] },
+             status: :not_found
+    when JWT::ExpiredSignature
+      render json: { errors: ['Authentication Error: Token has expired'] }, status: :unauthorized
+    when JWT::DecodeError
+      render json: { errors: ['Authentication Error: Invalid or Missing token'] },
+             status: :unauthorized
+    else
+      render json: { error: respond_with(error.message) },
+             status: :internal_server_error
     end
   end
 
   def respond_with(message)
     case message
     when /empty: first_name/
-      'First name parameter is required and cannot be empty'
+      ['First name parameter is required and cannot be empty']
     when /empty: last_name/
-      'Last name parameter is required and cannot be empty'
+      ['Last name parameter is required and cannot be empty']
     when /empty: email/
-      'Email parameter is required and cannot be empty'
+      ['Email parameter is required and cannot be empty']
     when /empty: password/
-      'Password parameter is required and cannot be empty'
+      ['Password parameter is required and cannot be empty']
     else
-      'An error has occurred on the server, please try again later'
+      ['An error has occurred on the server, please try again later']
     end
   end
 end
