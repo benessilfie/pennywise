9865fe3cf40f2575baea65628cef102a1f926c9f
benessilfie <bquansah007@gmail.com>
2023-02-27 23:26:04 +0000

    Added enpoints to submit pin and otp

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
     resource :user, only: %i[show create update]
 
     resource :wallet, only: %i[show]
-    resource :debit_card, only: %i[create destroy]
+    resource :debit_card, only: %i[create destroy] do
+      post :send_pin
+      post :send_otp
+    end
   end
 end
