2c1f51191213701416dabed4aa55ae37de535cce
benessilfie <bquansah007@gmail.com>
2023-03-02 23:50:15 +0000

    Added endpoints to make a debit or credit transfer from a wallet

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,12 @@ Rails.application.routes.draw do
     resource :session, only: %i[create destroy]
     resource :user, only: %i[show create update]
 
-    resource :wallet, only: %i[show]
+    resource :wallet, only: %i[show] do
+      post :credit
+      post :debit
+      post :transfer
+    end
+
     resource :debit_card, only: %i[create destroy] do
       post :send_pin
       post :send_otp
