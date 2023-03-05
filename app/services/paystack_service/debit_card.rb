5b60af272b689c410c71251634c601bd6a897292
benessilfie <bquansah007@gmail.com>
2023-03-05 23:43:35 +0000

    Updated required parameter name for paystack service

--- app/services/paystack_service/debit_card.rb
+++ app/services/paystack_service/debit_card.rb
@@ -9,29 +9,29 @@ class PaystackService::DebitCard
     }
   end
 
-  def self.charge(transaction:, card: nil, authorization: nil, pin: nil)
+  def self.charge(transaction:, card: nil, authorization_code: nil, pin: nil)
     body = {
       email: transaction.user.email,
       amount: transaction.amount,
       reference: transaction.transaction_reference
-    }.merge(card || {}).merge(authorization || {}).merge({ pin: } || {})
+    }.merge(card || {}).merge(authorization_code || {}).merge({ pin: } || {})
 
     post('/charge', body: body.to_json, headers:)
   end
 
-  def self.submit_pin(transaction:, pin:)
+  def self.submit_pin(reference:, pin:)
     body = {
       pin:,
-      reference: transaction.transaction_reference
+      reference:
     }
 
     post('/charge/submit_pin', body: body.to_json, headers:)
   end
 
-  def self.submit_otp(transaction:, otp:)
+  def self.submit_otp(reference:, otp:)
     body = {
       otp:,
-      reference: transaction.transaction_reference
+      reference:
     }
 
     post('/charge/submit_otp', body: body.to_json, headers:)
