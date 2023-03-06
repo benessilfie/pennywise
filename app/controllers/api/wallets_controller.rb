845f6b81d66a27bbeffe18778c5eb89d536cc5a4
benessilfie <bquansah007@gmail.com>
2023-03-06 23:38:45 +0000

    Updated wallet response

--- app/controllers/api/wallets_controller.rb
+++ app/controllers/api/wallets_controller.rb
@@ -1,5 +1,20 @@
 class Api::WalletsController < ApiController
+  include PinOtpSendable
+  include WalletHelper
+
+  before_action :set_transaction, only: %i[send_pin send_otp]
+
   def show
-    render json: current_user.wallet.response, status: :ok
+    render json: wallet.response, status: :ok
+  end
+
+  def credit
+    amount = params.require(:amount).to_f
+    debit_card = current_user.debit_cards.find(params.require(:card_id))
+
+    @transaction = wallet.make_credit(amount, source: debit_card)
+    response = charge_debit_card(debit_card, @transaction)
+
+    paystack_response(response)
   end
 end
